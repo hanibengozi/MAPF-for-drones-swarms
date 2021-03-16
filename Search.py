0258381df4953a7c0576952df827175163ea1c10
Yehudit <judy20925@gmail.com>
2021-03-17 00:54:07 +0200

    second mission write the function get_possible_steps(), that return the next free steps ordered by priority

--- Search.py
+++ Search.py
@@ -1,3 +1,5 @@
+import queue
+
 from pip._vendor.msgpack.fallback import xrange
 
 
@@ -31,6 +33,8 @@ def find_path(agents, world):
             paths[agent].append(current_step)                         # update the path for this agent
 
             if current_step != goal:                                       # in case we didnt arrived to the goal pos, keep loking for the next step
+                steps_queue = get_possible_steps(current_step, goal, current_steps, prev_steps, world)
+                print("time: ", i, " current step: ", current_step," goal: ", goal, " next_step: ", steps_queue.get())
                 current_step = getNextStep(current_step, goal)             # find the next step
 
                 if checkConflict(current_step, current_steps,prev_steps):  # Check if this step conflicts with other steps
@@ -63,3 +67,61 @@ def checkConflict(step, current_steps, prev_steps):
     if step in current_steps or step in prev_steps:              #in case their a conflict return 1
         return 1
     return 0
+
+# the method get step, and dict of steps and check that their are no conflict in this step, retuen True in case the step free.
+def check_free_step(step, steps):
+    if step not in steps:
+        return True
+    return False
+
+#the method get step, and return the next free step to goal
+def get_possible_steps(step, goal, current_steps, prev_steps, world):
+
+    possible_steps = queue.Queue()
+
+    steps = current_steps + list(set(prev_steps) - set(current_steps))    #put the current and prev steps in pne list, without duplicates
+
+    # add to queue all the free steps that are in the right direction.
+    for coordinate in range(2, -1, -1):
+        if step[coordinate] != goal[coordinate]:                          # check if the coordinate not in the right place already.
+            direction = 1 if step[coordinate] < goal[coordinate] else -1  # Sets the direction to the goal, up (=1) or down(=-1)
+            next_step = list(step)
+            next_step[coordinate] += direction                            # get the next step
+            if check_free_step(tuple(next_step), steps):                  # if this step free add to queue
+                possible_steps.put(tuple(next_step))
+
+    # add the rest of the steps to queue
+    for coordinate in range(2, -1, -1):
+        direction = 1 if step[coordinate] < goal[coordinate] else -1      # Sets the direction to the goal, up (=1) or down(=-1)
+        next_step = list(step)
+        next_step[coordinate] += direction
+        if tuple(next_step) not in possible_steps.queue:                  # check if this next step not already in the queue.
+            if check_free_step(tuple(next_step), steps):                  # check if this step is free.
+                if world.is_valid_pos(tuple(next_step)):                  # check if the step is valid
+                    possible_steps.put(tuple(next_step))
+
+        next_step = list(step)                                            # check for step in the opposite direction
+        next_step[coordinate] -= direction
+        if tuple(next_step) not in possible_steps.queue:                  # check if this next step not already in the queue.
+            if check_free_step(tuple(next_step), steps):                  # check if this step is free.
+                if world.is_valid_pos(tuple(next_step)):  # check if the step is valid
+                    possible_steps.put(tuple(next_step))
+
+    return possible_steps
+
+
+#def get_next_step(drones, index, current_steps):
+#if(index> drones.length)
+
+   #current_steps = copy_curren
+  #  return true
+# steps = getPossibleSteps(drones[index], current_steps) # get all steps that without collision
+# steps.count == 0
+#   return false
+#  for(step in steps)
+#     copy_current_step = current_steps.copy()
+#    update_step(step, copy_current_steps)
+#    if(getNextStep(array, index++, copy_current_steps))
+#       return true
+
+#  return false
