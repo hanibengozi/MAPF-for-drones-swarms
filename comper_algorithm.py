6c844d2ac83c12ca55468ae68a06c74662491f4f
Yehudit <judy20925@gmail.com>
2021-04-07 13:32:26 +0300

    fix bags

--- comper_algorithm.py
+++ comper_algorithm.py
@@ -1,5 +1,7 @@
-import algorithm_1.algoritm1_test
-import algorithm_2.algorithem2_test
+from matplotlib import pylab
+
+import algorithm_1.algoritm1_test as test_1
+import algorithm_2.algorithem2_test as test_2
 import matplotlib.pyplot as plt
 import numpy as np
 import time
@@ -22,11 +24,11 @@ NUM_CASES = 1
 def compare_run_time(agents_pos):
 
     start_time = time.time()
-    path_1, broblem_drones = algorithm_1.algoritm1_test.run_one_case(HEIGHT, WIDTH, LENGTH, AGENT_RADIUS, SECURITY_DISTANCE, agents_pos.copy())
+    path_1, broblem_drones = test_1.run_one_case(HEIGHT, WIDTH, LENGTH, AGENT_RADIUS, SECURITY_DISTANCE, agents_pos.copy())
     run_time_1 = time.time() - start_time
 
     start_time = time.time()
-    path_2 = algorithm_2.algorithem2_test.run_one_case(num_floors, num_rows, num_cols, agents_pos.copy())
+    path_2 = test_2.run_one_case(num_floors, num_rows, num_cols, agents_pos.copy())
     run_time_2 = time.time() - start_time
 
     return run_time_1, run_time_2
@@ -53,6 +55,7 @@ def draw_graph_run_time(x1, y1, x2, y2):
     plt.savefig("run_time_graph.png")
 
 def draw_graph_pathlen(x1, y1, x2, y2):
+    pylab.clf()
 
     # plotting the line 1 points
     plt.plot(x1, y1, label="algorithm 1")
@@ -72,6 +75,21 @@ def draw_graph_pathlen(x1, y1, x2, y2):
     # function to show the plot
     plt.savefig("pathlen_graph.png")
 
+def compare_2_algorithm(paths_1, broblem_cases, paths_2):
+
+    if paths_1 and not broblem_cases:             # in case algorithm 1 return paths
+        if paths_2:                               # in case algorithm 2 return paths
+            # compare with algorithm 1 test function
+            paths_2_dict = test_2.convert_paths(paths_2)
+            #if test_1.check_conflicts()
+
+            # compare with algorithm 2 test function
+
+        else:
+            print("algorithm 2 did not find path for this case")
+    else:
+        print("algorithm 1 did not find path for this case")
+
 
 def main():
     run_time_1 = run_time_2 = 0
@@ -81,26 +99,29 @@ def main():
     run_time_y2 = []
     max_path_len_1_list = []
     max_path_len_2_list = []
-    x = [ i for i in range(50, 101, 25)]
+    x = [ i for i in range(50, 101, 10)]
     for num_of_agents in x:
         run_time_1 = run_time_2 = 0
         success_1 = success_2 = 0
+        max_path_len_1 = max_path_len_2 = 0
 
         for i in range(NUM_CASES):
-            agents_pos = algorithm_1.algoritm1_test.random_inputs(num_floors, num_cols, num_rows, num_of_agents)
+            agents_pos = test_1.random_inputs(num_floors, num_cols, num_rows, num_of_agents)
             # find the paths for the agents pos, from each algorithm
-            path_1, broblem_drones = algorithm_1.algoritm1_test.run_one_case(HEIGHT, WIDTH, LENGTH, AGENT_RADIUS,SECURITY_DISTANCE, agents_pos.copy())
-            path_2 = algorithm_2.algorithem2_test.run_one_case(num_floors, num_rows, num_cols, agents_pos.copy())
+            path_1, broblem_drones = test_1.run_one_case(HEIGHT, WIDTH, LENGTH, AGENT_RADIUS,SECURITY_DISTANCE, agents_pos.copy())
+            path_2 = test_2.run_one_case(num_floors, num_rows, num_cols, agents_pos.copy())
 
             if path_1 and not broblem_drones:
                 success_1 += 1
-                max_path_len_1 += algorithm_1.algoritm1_test.get_max_pathlen(path_1)
+                max_path_len_1 += test_1.get_max_pathlen(path_1)
 
-            if algorithm_2.algorithem2_test.check_path(path_2):
+            if test_2.check_path(path_2):
                 success_2 += 1
-                path_2_dict = algorithm_2.algorithem2_test.convert_paths(path_2)
-                print(path_2_dict)
-                max_path_len_2 += algorithm_1.algoritm1_test.get_max_pathlen(path_2_dict)
+                path_2_dict = test_2.convert_paths(path_2)
+                #print(path_2_dict)
+                max_path_len_2 += test_1.get_max_pathlen(path_2_dict)
+            else:
+                print("in this agent pos their are problem in algorithm 2\n", agents_pos)
 
             # find time run of each algorithm for this cases
             time_1, time_2 = compare_run_time(agents_pos)
@@ -123,6 +144,7 @@ def main():
 
     # draw graph
     print(x, run_time_y1, run_time_y2)
+    print(max_path_len_1_list, max_path_len_2_list)
     draw_graph_run_time(x, run_time_y1, x, run_time_y2)
     draw_graph_pathlen(x, max_path_len_1_list, x, max_path_len_2_list)
 
